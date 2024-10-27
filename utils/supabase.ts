962f7f02e117b9047b1793cba5c0f1c215620d05
vherasymenko <manqarium@gmail.com>
2024-10-27 16:32:24 +0200

    small refactor

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const bucketID = 'home-away-b'
+const bucketID = process.env.BUCKET_ID as string
 const url = process.env.SUPABASE_URL as string
 const key = process.env.SUPABASE_KEY as string
 const supabase = createClient(url, key)
