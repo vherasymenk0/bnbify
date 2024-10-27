0d53421fdc191285f626fe70326d8e78122bee4c
vherasymenko <manqarium@gmail.com>
2024-10-27 14:06:22 +0200

    add card skeleton

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,25 @@
 import CategoriesList from '~/components/home/CategoriesList'
 import PropertiesContainer from '~/components/home/PropertiesContainer'
+import LoadingCards from '~/components/card/LoadingCards'
+import { Suspense } from 'react'
 
 function HomePage({
   searchParams,
 }: {
-  searchParams: { category?: string, search?: string }
+  searchParams: { category?: string; search?: string };
 }) {
-  // console.log(searchParams);
-
   return (
     <section>
       <CategoriesList
         category={searchParams?.category}
         search={searchParams?.search}
       />
-      <PropertiesContainer
-        category={searchParams?.category}
-        search={searchParams?.search}
-      />
+      <Suspense fallback={<LoadingCards/>}>
+        <PropertiesContainer
+          category={searchParams?.category}
+          search={searchParams?.search}
+        />
+      </Suspense>
     </section>
   )
 }
